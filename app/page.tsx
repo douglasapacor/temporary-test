2383a7fb48b823b7572da146f29326f60cb99715
Douglas Pacor <douglas@epicquestti.com.br>
2025-12-04 19:16:05 -0300

    first settings done

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
+import { useEffect } from "react";
+
 export default function Page() {
+  useEffect(() => {
+    console.log("çalskdhasdlkhdkjahdkajshdkajdh");
+  }, []);
   return <div>page</div>;
 }
